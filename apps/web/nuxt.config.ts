104557fc4abcc518a9fdf77606bc1c21dc878805
jlemonz <112578448+jlemonz@users.noreply.github.com>
2026-07-19 23:44:41 +0300

    u

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@layers-bug/base'],
+  modules: ['@nuxtjs/supabase'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
